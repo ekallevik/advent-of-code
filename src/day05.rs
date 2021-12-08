4b77209a263f6d6189ebf91db806d78124ac16ad
Even Kallevik <even.kallevik@gmail.com>
2021-12-08 19:24:29 +0100

    WIP: Solve day 5

--- src/day05.rs
+++ src/day05.rs
@@ -1,57 +1,24 @@
 use crate::domain::Line;
-use crate::utils::{get_input, PuzzlePart};
+use crate::utils::get_input;
 
 
+pub fn solve_1(filename: String) -> String {
 
-pub fn solve(part: PuzzlePart) -> u64 {
-    println!("Puzzle day 05 - {:?}", part);
-    let input = get_input("src/input05.txt");
-    match part {
-        PuzzlePart::Part1 => solve_part_1(input),
-        PuzzlePart::Part2 => solve_part_2(input),
-    }
-}
+    let input: Vec<Line> = get_input(filename);
 
-fn solve_part_1(input: Vec<Line>) -> u64 {
     let size = input
         .iter()
         .map(|line| (line).largest_point())
         .max()
         .unwrap();
 
-    let ocean = vec![vec![0; size as usize]; size as usize];
+    let _ocean = vec![vec![0; size as usize]; size as usize];
 
-    for line in input.iter() {}
+    for _line in input.iter() {}
 
-    23
+    "-1".to_string()
 }
 
-fn solve_part_2(input: Vec<Line>) -> u64 {
-    solve_part_1(input)
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    
-
-    #[test]
-    fn test_solve_part_1() {
-        let input = get_input("src/input05_test.txt");
-
-        let expected = 5934;
-
-        let result = solve_part_1(input);
-        assert_eq!(result, expected);
-    }
-
-    #[test]
-    fn test_solve_part_2() {
-        let input = get_input("src/input05_test.txt");
-
-        let expected = 26984457539;
-
-        let result = solve_part_2(input);
-        assert_eq!(result, expected)
-    }
+pub fn solve_2(filename: String) -> String {
+    solve_1(filename)
 }
