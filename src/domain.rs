afa2402b95bd0e297424f4ac4f673c5d88df3dd5
Even Kallevik <even.kallevik@gmail.com>
2021-12-08 19:24:29 +0100

    WIP: Solve day 5

--- src/domain.rs
+++ src/domain.rs
@@ -1,4 +1,36 @@
+use std::cmp::max;
 use std::str::FromStr;
+
+#[derive(Debug)]
+pub struct Line {
+    pub start: NaivePosition,
+    pub end: NaivePosition,
+}
+
+impl FromStr for Line {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let pairs: Vec<&str> = s.split(" -> ").collect();
+
+        Ok(Line {
+            start: pairs[0].parse().unwrap(),
+            end: pairs[0].parse().unwrap(),
+        })
+    }
+}
+
+impl Line {
+    pub fn largest_point(&self) -> i64 {
+        let max_x = max(self.start.x, self.end.x);
+        let max_y = max(self.start.y, self.end.y);
+        max(max_x, max_y)
+    }
+
+    pub fn get_start(&self) -> &NaivePosition {
+        &self.start
+    }
+}
 
 #[derive(Debug)]
 pub enum SubmarineCommand {
@@ -24,12 +56,11 @@ impl FromStr for SubmarineCommand {
 }
 
 impl SubmarineCommand {
-
     pub fn naive_apply_from(&self, point: NaivePosition) -> NaivePosition {
         match self {
             SubmarineCommand::Up(value) => point.up(value),
             SubmarineCommand::Down(value) => point.down(value),
-            SubmarineCommand::Forward(value) => point.forward(value)
+            SubmarineCommand::Forward(value) => point.forward(value),
         }
     }
 
@@ -37,28 +68,50 @@ impl SubmarineCommand {
         match self {
             SubmarineCommand::Up(value) => position.up(value),
             SubmarineCommand::Down(value) => position.down(value),
-            SubmarineCommand::Forward(value) => position.forward(value)
+            SubmarineCommand::Forward(value) => position.forward(value),
         }
     }
 }
 
+#[derive(Debug)]
 pub struct NaivePosition {
     pub x: i64,
     pub y: i64,
 }
 
-impl NaivePosition {
+impl FromStr for NaivePosition {
+    type Err = ();
 
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let numbers: Vec<i64> = s.split(',').map(|n| n.parse().unwrap()).collect();
+
+        Ok(NaivePosition {
+            x: numbers[0],
+            y: numbers[1],
+        })
+    }
+}
+
+impl NaivePosition {
     fn up(&self, value: &i64) -> NaivePosition {
-        NaivePosition { x: self.x, y: self.y + value}
+        NaivePosition {
+            x: self.x,
+            y: self.y + value,
+        }
     }
 
     fn down(&self, value: &i64) -> NaivePosition {
-        NaivePosition { x: self.x, y: self.y - value}
+        NaivePosition {
+            x: self.x,
+            y: self.y - value,
+        }
     }
 
     fn forward(&self, value: &i64) -> NaivePosition {
-        NaivePosition { x: self.x + value, y: self.y }
+        NaivePosition {
+            x: self.x + value,
+            y: self.y,
+        }
     }
 }
 
@@ -69,20 +122,27 @@ pub struct Position {
 }
 
 impl Position {
-
     fn up(&self, value: &i64) -> Position {
-        Position { x: self.x, y: self.y, aim: self.aim - value }
+        Position {
+            x: self.x,
+            y: self.y,
+            aim: self.aim - value,
+        }
     }
 
     fn down(&self, value: &i64) -> Position {
-        Position { x: self.x, y: self.y, aim: self.aim + value }
+        Position {
+            x: self.x,
+            y: self.y,
+            aim: self.aim + value,
+        }
     }
 
     fn forward(&self, value: &i64) -> Position {
         Position {
             x: self.x + value,
-            y: self.y + value*self.aim,
-            aim: self.aim
+            y: self.y + value * self.aim,
+            aim: self.aim,
         }
     }
 }
