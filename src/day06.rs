89602f31ccca51c02ec84191bedc0ecf273ba26c
Even Kallevik <even.kallevik@gmail.com>
2021-12-08 19:22:23 +0100

    Solve day 6

--- src/day06.rs
+++ src/day06.rs
@@ -1,8 +1,15 @@
-use crate::utils::{PuzzlePart};
 
 
-pub fn solve(part: PuzzlePart) -> u64 {
-    println!("Puzzle day 0 - {:?}", part);
+pub fn solve_1(_filename: String) -> String {
+    solve(_filename, 80)
+}
+
+pub fn solve_2(_filename: String) -> String {
+    solve(_filename, 256)
+}
+
+fn solve(_filename: String, days: u16) -> String {
+
     let input = vec![
         4, 1, 4, 1, 3, 3, 1, 4, 3, 3, 2, 1, 1, 3, 5, 1, 3, 5, 2, 5, 1, 5, 5, 1, 3, 2, 5, 3, 1, 3,
         4, 2, 3, 2, 3, 3, 2, 1, 5, 4, 1, 1, 1, 2, 1, 4, 4, 4, 2, 1, 2, 1, 5, 1, 5, 1, 2, 1, 4, 4,
@@ -15,13 +22,7 @@ pub fn solve(part: PuzzlePart) -> u64 {
         2, 2, 1, 4, 1, 3, 4, 2, 1, 1, 5, 4, 1, 5, 4, 4, 3, 1, 3, 3, 1, 1, 3, 3, 4, 2, 3, 4, 2, 3,
         1, 4, 1, 5, 3, 1, 1, 5, 3, 2, 3, 5, 1, 3, 1, 1, 3, 5, 1, 5, 1, 1, 3, 1, 1, 1, 1, 3, 3, 1,
     ];
-    match part {
-        PuzzlePart::Part1 => solve_part_1(input, 80),
-        PuzzlePart::Part2 => solve_part_2(input, 256),
-    }
-}
 
-fn solve_part_1(input: Vec<u64>, days: u16) -> u64 {
     let mut count = (0..=8)
         .map(|i| input.iter().filter(|&fish| *fish == i).count() as u64)
         .collect::<Vec<u64>>();
@@ -33,36 +34,5 @@ fn solve_part_1(input: Vec<u64>, days: u16) -> u64 {
         count.push(number_of_zeroes);
     }
 
-    count.iter().sum::<u64>()
-}
-
-fn solve_part_2(input: Vec<u64>, days: u16) -> u64 {
-    solve_part_1(input, days)
-}
-
-#[cfg(test)]
-mod tests {
-    use crate::utils::get_input;
-    use super::*;
-    
-
-    #[test]
-    fn test_solve_part_1() {
-        let input = get_input("src/input6_test.txt");
-
-        let expected = 5934;
-
-        let result = solve_part_1(input, 80);
-        assert_eq!(result, expected);
-    }
-
-    #[test]
-    fn test_solve_part_2() {
-        let input = get_input("src/input6_test.txt");
-
-        let expected = 26984457539;
-
-        let result = solve_part_2(input, 256);
-        assert_eq!(result, expected)
-    }
+    count.iter().sum::<u64>().to_string()
 }
