c4fa9a7f6a20775a879d808357279e94969fc338
Even Kallevik <even.kallevik@gmail.com>
2021-12-08 19:24:11 +0100

    WIP: Solve day 3

--- src/day03.rs
+++ src/day03.rs
@@ -1,17 +1,78 @@
-use crate::utils::{get_input, get_input_array, PuzzlePart};
+use crate::utils::{get_input, get_input_array};
 
-pub fn solve(part: PuzzlePart) -> i64 {
-    println!("Puzzle day 03 - {:?}", part);
-    let input_a: Vec<Vec<u8>> = get_input_array("src/input03.txt");
-    let input: Vec<String> = get_input("src/input03.txt");
-    let input2 = get_input("src/input03.txt");
+pub fn solve_1(filename: String) -> String {
+    let input = get_input_array(filename);
+
+    let size = input.len();
+    let length = input.first().unwrap().len();
+
+    let ones = count_number_of_ones(input);
+    let gamma = calculate_gamma(ones, size, length);
+
+    let max_value = 2i64.pow(length as u32) - 1;
+    let epsilon = max_value - gamma;
+
+    (epsilon * gamma).to_string()
+}
+
+pub fn solve_2(filename: String) -> String {
+    let input_a: Vec<Vec<u8>> = get_input_array(filename.clone());
+    let input2 = get_input(filename.clone());
+    let input: Vec<String> = get_input(filename);
 
     let length = input.first().unwrap().len();
 
-    match part {
-        PuzzlePart::Part1 => solve_part_1(input_a),
-        PuzzlePart::Part2 => solve_part_2(input, input2, length, input_a),
+    let mut oxygen_list: Vec<Vec<char>> = convert_input(input);
+    let mut co2_list: Vec<Vec<char>> = convert_input(input2);
+
+    let mut oxygen = 0;
+    let mut co2 = 0;
+
+    let ox_list = input_a;
+    let size = ox_list.first().unwrap().len();
+    let ox = filter_inputs(ox_list, length, size);
+
+    for i in 0..length {
+        let most_common_bit = find_most_common_bit_from_char(&oxygen_list, i);
+        oxygen_list = oxygen_list
+            .into_iter()
+            .filter(|elem| elem[i] == most_common_bit)
+            .collect();
+
+        if oxygen_list.len() == 1 {
+            for i in 0..length {
+                if oxygen_list[0][i] == '1' {
+                    oxygen += 2i64.pow((length - i - 1) as u32);
+                }
+            }
+        }
     }
+
+    for i in 0..length {
+        let most_common_bit = find_most_common_bit_from_char(&co2_list, i);
+        co2_list = co2_list
+            .into_iter()
+            .filter(|elem| elem[i] != most_common_bit)
+            .collect();
+
+        if co2_list.len() == 1 {
+            for i in 0..length {
+                if co2_list[0][i] == '1' {
+                    co2 += 2i64.pow((length - i - 1) as u32);
+                }
+            }
+            println!(
+                "\n####\nTarget: {:?} with value={:?}\n####\n",
+                oxygen_list, oxygen
+            );
+        }
+    }
+
+    if ox != oxygen {
+        panic!("Oxygen: {}, ox: {}", oxygen, ox);
+    }
+
+    (oxygen * co2).to_string()
 }
 
 fn count_ones(lines: &Vec<Vec<u8>>, index: usize) -> usize {
@@ -89,18 +150,6 @@ fn calculate_gamma2(frequencies: Vec<u8>, size: usize, length: usize) -> i64 {
     res
 }
 
-fn solve_part_1(input: Vec<Vec<u8>>) -> i64 {
-    let size = input.len();
-    let length = input.first().unwrap().len();
-
-    let ones = count_number_of_ones(input);
-    let gamma = calculate_gamma(ones, size, length);
-
-    let max_value = 2i64.pow(length as u32) - 1;
-    let epsilon = max_value - gamma;
-
-    epsilon * gamma
-}
 
 fn find_most_common_bit_from_char(lines: &Vec<Vec<char>>, i: usize) -> char {
     let zeros = lines.iter().filter(|&x| x[i] == '0').count();
@@ -146,92 +195,4 @@ fn filter_inputs(input_a: Vec<Vec<u8>>, length: usize, size: usize) -> i64 {
     }
 
     value
-}
-
-fn solve_part_2(
-    input: Vec<String>,
-    input2: Vec<String>,
-    length: usize,
-    input_a: Vec<Vec<u8>>,
-) -> i64 {
-    let mut oxygen_list: Vec<Vec<char>> = convert_input(input);
-    let mut co2_list: Vec<Vec<char>> = convert_input(input2);
-
-    let mut oxygen = 0;
-    let mut co2 = 0;
-
-    let ox_list = input_a;
-    let size = ox_list.first().unwrap().len();
-    let ox = filter_inputs(ox_list, length, size);
-
-    for i in 0..length {
-        let most_common_bit = find_most_common_bit_from_char(&oxygen_list, i);
-        oxygen_list = oxygen_list
-            .into_iter()
-            .filter(|elem| elem[i] == most_common_bit)
-            .collect();
-
-        if oxygen_list.len() == 1 {
-            for i in 0..length {
-                if oxygen_list[0][i] == '1' {
-                    oxygen += 2i64.pow((length - i - 1) as u32);
-                }
-            }
-        }
-    }
-
-    for i in 0..length {
-        let most_common_bit = find_most_common_bit_from_char(&co2_list, i);
-        co2_list = co2_list
-            .into_iter()
-            .filter(|elem| elem[i] != most_common_bit)
-            .collect();
-
-        if co2_list.len() == 1 {
-            for i in 0..length {
-                if co2_list[0][i] == '1' {
-                    co2 += 2i64.pow((length - i - 1) as u32);
-                }
-            }
-            println!(
-                "\n####\nTarget: {:?} with value={:?}\n####\n",
-                oxygen_list, oxygen
-            );
-        }
-    }
-
-    if ox != oxygen {
-        panic!("Oxygen: {}, ox: {}", oxygen, ox);
-    }
-
-    oxygen * co2
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use crate::utils::get_input_array;
-
-    #[test]
-    fn test_solve_part_1() {
-        let input: Vec<Vec<u8>> = get_input_array("src/input03_test.txt");
-
-        let expected = 198;
-
-        let result = solve_part_1(input);
-        assert_eq!(result, expected);
-    }
-
-    #[test]
-    fn test_solve_part_2() {
-        let input: Vec<String> = get_input("src/input03_test.txt");
-        let input2 = get_input("src/input03_test.txt");
-        let input_a = get_input_array("src/input03_test.txt");
-        let length = input.first().unwrap().len();
-
-        let expected = 230;
-
-        let result = solve_part_2(input, input2, length, input_a);
-        assert_eq!(result, expected)
-    }
 }
