b577c386498c2c7e65b0baa46e6fb8d9ce59d41d
Even Kallevik <even.kallevik@gmail.com>
2021-12-18 11:23:43 +0100

    Add template

---
+++ src/template.rs
@@ -0,0 +1,14 @@
+use crate::utils::get_input_array;
+
+// todo: save as template
+pub fn solve_1(filename: &str) -> String {
+    let _input: Vec<Vec<usize>> = get_input_array(filename);
+
+    unimplemented!("Not implemented yet");
+}
+
+pub fn solve_2(filename: &str) -> String {
+    let _input: Vec<Vec<usize>> = get_input_array(filename);
+
+    unimplemented!("Not implemented yet");
+}
