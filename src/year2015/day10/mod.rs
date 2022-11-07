b86747395d2cbd7ba7be4de42196f1d2adce94a3
Even Kallevik <even.kallevik@gmail.com>
2022-11-07 12:04:06 +0100

    Solve 2015-10

---
+++ src/year2015/day10/mod.rs
@@ -0,0 +1,46 @@
+use itertools::PeekingNext;
+use paris::info;
+use crate::utils::get_input_string;
+
+pub fn solve_1(filename: &str) -> String {
+    let input = get_input_string(filename);
+    let output = iterate_look_and_say(input, 50);
+
+    output.len().to_string()
+}
+
+pub fn solve_2(filename: &str) -> String {
+    let input = get_input_string(filename);
+    let output = iterate_look_and_say(input, 50);
+
+    output.len().to_string()
+}
+
+fn iterate_look_and_say(mut input: String, iterations: usize) -> String {
+    for i in 0..iterations {
+        info!("iteration: {i}");
+        input = look_and_say(&input);
+    }
+
+    input
+}
+
+fn look_and_say(input: &str) -> String {
+
+    let mut chars = input.chars();
+    let mut output = "".to_string();
+
+    while let Some(char) = chars.next() {
+
+        let mut counter = 1;
+
+        while chars.peeking_next(|p| *p == char).is_some() {
+            counter += 1;
+        }
+
+        output.push_str(counter.to_string().as_str());
+        output.push(char);
+    }
+
+    output
+}
