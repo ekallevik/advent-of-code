46437a0ee142c77862c496dc2496e83cc8ba6784
Even Kallevik <even.kallevik@gmail.com>
2022-11-15 16:34:16 +0100

    Lint source

--- src/year2021/day24/source.rs
+++ src/year2021/day24/source.rs
@@ -3,22 +3,6 @@ use std::str::FromStr;
 #[derive(Clone, Copy, Debug)]
 pub enum Source { Reg(usize), Val(i64) }
 
-impl Source {
-    pub fn val(&self, regs: &[i64; 4]) -> i64 {
-        match *self {
-            Self::Reg(i) => regs[i],
-            Self::Val(v) => v,
-        }
-    }
-
-    pub fn get(&self) -> usize {
-        match self {
-            Source::Reg(v) => *v,
-            Source::Val(v) => *v as usize,
-        }
-    }
-}
-
 impl FromStr for Source {
     type Err = ();
 
