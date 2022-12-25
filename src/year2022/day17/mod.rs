ac185d40a51c15721769e44ba877bbb4191fbdba
Even Kallevik <even.kallevik@gmail.com>
2022-12-25 11:23:06 +0100

    Solve 2022-17 - first

--- src/year2022/day17/mod.rs
+++ src/year2022/day17/mod.rs
@@ -5,7 +5,7 @@ use indicatif::{ProgressBar, ProgressStyle};
 use itertools::Itertools;
 use crate::utils::{get_input, get_input_string};
 
-#[derive(Debug)]
+#[derive(Debug, Eq, PartialEq)]
 enum Rock {
     Horizontal,
     Plus,
@@ -70,7 +70,7 @@ pub fn solve_1(filename: &str) -> Result<String> {
     let mut grid: HashSet<Position> = (0..width).into_iter().map(|w| (w, 0)).collect();
 
     let bar = ProgressBar::new(rounds);
-    bar.set_style(ProgressStyle::with_template("elapsed: [{elapsed_precise}] eta: [{eta_precise}] {bar:40.cyan/blue} {pos:>7}/{len:7} {percent}")
+    bar.set_style(ProgressStyle::with_template("{bar:40.cyan/blue} {pos:>7}/{len:7} {msg}")
         .unwrap());
 
     for _ in 1..=rounds {
@@ -79,8 +79,6 @@ pub fn solve_1(filename: &str) -> Result<String> {
 
         let rock = &rocks[0];
 
-        //println!("\n\nRound {} - starting height = {}", round, height);
-
         loop {
             let absolute_positions = rock
                 .get_positions()
@@ -88,13 +86,6 @@ pub fn solve_1(filename: &str) -> Result<String> {
                 .map(|relative| (relative.0 + rock_width, relative.1 + rock_height))
                 .collect_vec();
             let push = &pushes[0];
-
-            /*
-            println!("\nRock {:?} at pos: ({}, {})", rock, rock_width, rock_height);
-            print_grid(&grid, width, rock_height+4, &absolute_positions);
-
-
-             */
 
             let can_push = absolute_positions
                 .iter()
@@ -107,7 +98,6 @@ pub fn solve_1(filename: &str) -> Result<String> {
                 .all(|p| p.0 < width && !grid.contains(&p));
 
             let pushed_positions = if can_push {
-
                 match push {
                     Push::Left => rock_width -= 1,
                     Push::Right => rock_width += 1,
@@ -121,14 +111,10 @@ pub fn solve_1(filename: &str) -> Result<String> {
                     };
                     res.push(pushed);
                 }
-                //println!("Pushed one unit to the {:?}", push);
                 res
             } else {
-                //println!("Not able to push");
                 absolute_positions
             };
-            //print_grid(&grid, width, rock_height+4, &pushed_positions);
-
 
             pushes.rotate_left(1);
 
@@ -153,17 +139,11 @@ pub fn solve_1(filename: &str) -> Result<String> {
             } else {
                 rock_height -= 1;
             }
-            bar.inc(1);
         }
 
+        bar.inc(1);
+        bar.set_message(format!("Height: {}", height));
         rocks.rotate_left(1);
-
-        /*
-        println!("\n\nRound {} - ending height = {}", round, height);
-        print_grid(&grid, width, height, &vec![]);
-        println!("\n\n");
-
-         */
     }
 
     bar.finish();
@@ -171,10 +151,153 @@ pub fn solve_1(filename: &str) -> Result<String> {
     Ok(height.to_string())
 }
 
-
 pub fn solve_2(filename: &str) -> Result<String> {
-    let round = 1;
-    Ok(round.to_string())
+    let mut input = get_input_string(filename);
+    let mut pushes = input.chars().map(|c| c.to_string().parse().unwrap()).collect_vec();
+
+    let width = 7;
+    let spawn_width = 2;
+    let height_delta = 4;
+
+    let mut rocks = [
+        Rock::Horizontal,
+        Rock::Plus,
+        Rock::Angle,
+        Rock::Vertical,
+        Rock::Block
+    ];
+
+    let rounds: isize = 1000000000000;
+    let mut height = 0;
+    let checkpoint = pushes.len();
+
+    let mut grid: HashSet<Position> = (0..width).into_iter().map(|w| (w, 0)).collect();
+
+    let bar = ProgressBar::new(rounds as u64);
+    bar.set_style(ProgressStyle::with_template("elapsed: [{elapsed_precise}] eta: [{eta_precise}] {bar:40.cyan/blue} {pos:>7}/{len:7} {msg}")
+        .unwrap());
+
+    println!("Checkpoint: {}, pushes: {}, rocks: {}", checkpoint, pushes.len(), rocks.len());
+
+    let mut increases = vec![];
+    let mut round = 0;
+    let mut first_cycle_round = None;
+    let mut cycle_size = None;
+    let mut number_of_rocks_in_cycle = 0;
+
+    loop {
+        let mut rock_height = height + height_delta;
+        let mut rock_width = spawn_width;
+
+        let rock = &rocks[0];
+
+        loop {
+            let absolute_positions = rock
+                .get_positions()
+                .iter()
+                .map(|relative| (relative.0 + rock_width, relative.1 + rock_height))
+                .collect_vec();
+            let push = &pushes[0];
+
+            let can_push = absolute_positions
+                .iter()
+                .map(|p|
+                    match push {
+                        Push::Left => (p.0 - 1, p.1),
+                        Push::Right => (p.0 + 1, p.1),
+                    }
+                )
+                .all(|p| p.0 < width && !grid.contains(&p));
+
+            let pushed_positions = if can_push {
+                match push {
+                    Push::Left => rock_width -= 1,
+                    Push::Right => rock_width += 1,
+                }
+
+                let mut res = vec![];
+                for mut position in absolute_positions {
+                    let pushed = match push {
+                        Push::Left => (position.0 - 1, position.1),
+                        Push::Right => (position.0 + 1, position.1),
+                    };
+                    res.push(pushed);
+                }
+                res
+            } else {
+                absolute_positions
+            };
+
+            pushes.rotate_left(1);
+
+            let cannot_fall = pushed_positions
+                .iter()
+                .map(|p| (p.0, p.1 - 1))
+                .any(|p| grid.contains(&p));
+
+            if cannot_fall {
+                for position in pushed_positions {
+                    grid.insert(position);
+                }
+
+                let highest = grid
+                    .iter()
+                    .max_by(|x, y| x.1.cmp(&y.1))
+                    .unwrap();
+
+                let height_delta = highest.1 - height;
+                increases.push(height_delta);
+                height = highest.1;
+
+                break;
+            } else {
+                rock_height -= 1;
+            }
+        }
+
+        bar.inc(1);
+        bar.set_message(format!("Round: {}, Height: {}", round, height));
+
+        round += 1;
+
+        if let Some(first_round) = first_cycle_round {
+
+            let size = cycle_size.unwrap() as isize;
+
+            if round == first_round + size {
+                //print_grid(&grid, width, height, &vec![]);
+
+                println!("round: {}, height: {}, rocks: {}", round, height, number_of_rocks_in_cycle);
+                let remaining_cycles = (rounds - round) / number_of_rocks_in_cycle;
+                let pattern_height = remaining_cycles * size;
+                println!("cycle size: {}, remaining cycles: {}, pattern_height: {}", size, remaining_cycles, pattern_height);
+
+                break;
+            }
+        } else if round > 10 {
+            if let Some(cycle_size_1) = is_offset_cyclic(&grid, height, width) {
+                println!("round: {}, height: {}", round, height);
+
+                cycle_size = Some(cycle_size_1);
+                first_cycle_round = Some(round);
+
+            }
+        }
+
+        if first_cycle_round.is_some() {
+            number_of_rocks_in_cycle += 1;
+        }
+
+        if round == rounds {
+            break;
+        }
+
+        rocks.rotate_left(1);
+    }
+
+    bar.finish();
+
+    Ok(height.to_string())
 }
 
 
@@ -203,5 +326,80 @@ fn print_grid(grid: &HashSet<Position>, width: usize, height: usize, rock: &[Pos
         } else {
             println!("|");
         }
+    }
+}
+
+fn is_offset_cyclic(grid: &HashSet<Position>, height: usize, width: usize) -> Option<usize> {
+    if height % 2 != 0 {
+        return None;
+    }
+
+    for segment_size in 20..100 {
+        if is_cyclic_segment(grid, height, width, segment_size) {
+
+            return Some(segment_size);
+        }
+    }
+
+
+    None
+}
+
+fn is_cyclic_segment(grid: &HashSet<Position>, height: usize, width: usize, cycle_size: usize) -> bool {
+    for w in 0..width {
+        for delta_h in 0..cycle_size {
+            let a = grid.contains(&(w, height - delta_h));
+            let b = grid.contains(&(w, height - cycle_size - delta_h));
+
+            if a != b {
+                return false;
+            }
+        }
+    }
+
+    true
+}
+
+fn is_cyclic(grid: &HashSet<Position>, height: usize, width: usize) -> bool {
+    if height % 2 != 0 {
+        return false;
+    }
+
+    let midpoint = height / 2;
+    //println!("mid: {midpoint}");
+
+    for w in 0..width {
+        for delta_h in 0..midpoint {
+
+            //println!("w: {w}, delta_h: {delta_h}");
+            let a = grid.contains(&(w, delta_h));
+            let b = grid.contains(&(w, midpoint + delta_h));
+
+
+            if a != b {
+                return false;
+            }
+        }
+    }
+
+    true
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_is_cyclic() {
+        let mut grid = HashSet::new();
+        grid.insert((2, 0));
+        grid.insert((3, 0));
+        grid.insert((4, 0));
+
+        grid.insert((2, 1));
+        grid.insert((3, 1));
+        grid.insert((4, 1));
+
+        assert!(is_cyclic(&grid, 2, 2))
     }
 }
