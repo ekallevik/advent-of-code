e027808b325db256915767d2bd8fa5ca6c0d8aba
Even Kallevik <even.kallevik@gmail.com>
2022-12-26 20:24:56 +0100

    Solve 2022-13

--- src/year2022/day13/mod.rs
+++ src/year2022/day13/mod.rs
@@ -1,161 +1,176 @@
-use std::str::FromStr;
+use std::cmp::Ordering;
+use std::fmt::{Display, Formatter};
+use std::str::{Chars, FromStr};
 use anyhow::Result;
 use itertools::Itertools;
-use crate::utils::get_input_string;
+use crate::utils::{get_input, get_input_string};
+use crate::utils::string::is_numeric;
+
+impl Display for Distress {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Distress::Array(value) => {
+                write!(f, "[").expect("");
+                for v in value {
+                    write!(f, "{}, ", v).expect("");
+                }
+                write!(f, "]").expect("");
+            }
+            Distress::Number(number) => write!(f, "{}", number).expect("")
+        }
+        Ok(())
+    }
+}
 
 #[derive(Debug, PartialEq, Eq, Clone)]
 enum Distress {
     Array(Vec<Distress>),
-    Number(usize)
+    Number(usize),
+}
+
+impl PartialOrd<Self> for Distress {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        match (self, other) {
+            (Distress::Number(l), Distress::Number(r)) => l.partial_cmp(&r),
+            (Distress::Number(l), Distress::Array(_)) => {
+                let wrapped = Distress::Array(vec![Distress::Number(*l)]);
+                wrapped.partial_cmp(other)
+            }
+            (Distress::Array(_), Distress::Number(r)) => {
+                let wrapped = &Distress::Array(vec![Distress::Number(*r)]);
+                self.partial_cmp(wrapped)
+            }
+            (Distress::Array(left_array), Distress::Array(right_array)) => {
+                for (i, l) in left_array.iter().enumerate() {
+                    if let Some(r) = right_array.get(i) {
+                        let ordering = l.cmp(r);
+
+                        match ordering {
+                            Ordering::Less => return Some(Ordering::Less),
+                            Ordering::Equal => continue,
+                            Ordering::Greater => return Some(Ordering::Greater),
+                        }
+                    } else {
+                        return Some(Ordering::Greater);
+                    }
+                }
+
+                return if left_array.len() < right_array.len() {
+                    Some(Ordering::Less)
+                } else {
+                    Some(Ordering::Equal)
+                };
+            }
+        }
+    }
+}
+
+impl Ord for Distress {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.partial_cmp(other).unwrap()
+    }
 }
 
 impl FromStr for Distress {
     type Err = ();
 
     fn from_str(s: &str) -> std::result::Result<Self, Self::Err> {
+        let mut chars = s.chars();
+        chars.next();
 
-        let result = if let Ok(number) = s.parse::<usize>() {
-            Distress::Number(number)
-        } else if s.is_empty() {
-            Distress::Array(vec![])
-        }  else if !s.contains('[') {
-            println!("s={s}");
-            let arr = s
-                .trim()
-                .split(',')
-                .into_iter()
-                .map(|a| a.parse::<usize>().unwrap())
-                .map(Distress::Number)
-                .collect_vec();
-
-            Distress::Array(arr)
-        } else {
-            println!();
-
-            let content = s
-                .strip_prefix('[')
-                .unwrap()
-                .strip_suffix(']')
-                .unwrap();
-
-            if let Ok(number) = content.parse::<usize>() {
-                return Ok(Distress::Number(number))
-            }
-
-            let mut openings = 0;
-            let mut closings = 0;
-            let mut midpoint = 0;
-
-            for (i, c) in content.chars().enumerate() {
-                if c == '[' {
-                    openings += 1;
-                } else if c == ']' {
-                    closings += 1;
-                }
-
-                if c == ',' && openings == closings {
-                    midpoint = i;
-                }
-            }
-
-            println!("s={s}, content={content}\nmidpoint={midpoint:?}, openings={openings}, closings={closings}");
-            println!("start={}, end={}, openings={}", content.starts_with('['), content.ends_with(']'), openings == 1);
-
-            if openings == 1 {
-                println!("Found one array");
-
-                if content.starts_with('[') && content.ends_with(']') {
-                    println!("Enclosed arr");
-                    let c = content
-                        .strip_prefix('[')
-                        .unwrap()
-                        .strip_suffix(']')
-                        .unwrap();
-                    println!("c={c}");
-                    let distress = c.parse().unwrap();
-                    println!("value={distress:?}");
-                    Distress::Array(vec![distress])
-                } else if content.starts_with('[') {
-                    println!("Leading arr");
-                    let c = content
-                        .strip_prefix('[')
-                        .unwrap();
-
-                    let (left, right) = c.split_once("],").unwrap();
-                    println!("l={left}, r={right}");
-                    let l = left.parse().unwrap();
-                    let r = right.parse().unwrap();
-                    let arr = vec![l, r];
-                    Distress::Array(arr)
-                } else {
-                    println!("Trailing arr");
-                    let c = content
-                        .strip_suffix(']')
-                        .unwrap();
-
-                    let (left, right) = c.split_once(",[").unwrap();
-                    let l = left.parse().unwrap();
-                    let r = right.parse().unwrap();
-                    let arr = vec![l, r];
-                    Distress::Array(arr)
-                }
-
-
-            } else if openings > 0 {
-                println!("Found pair");
-                let (left, right_a) = content.split_at(midpoint);
-                let right = right_a.strip_prefix(',').unwrap();
-                println!("left={left}, right={right}\n");
-
-                let l = left.parse().unwrap();
-                let r = right.parse().unwrap();
-                let arr = vec![l, r];
-                Distress::Array(arr)
-            } else {
-                println!("Found else");
-                let arr = content
-                    .split(',')
-                    .into_iter()
-                    .map(|a| a.parse().unwrap())
-                    .collect_vec();
-
-                Distress::Array(arr)
-            }
-
-
-        };
-
-        Ok(result)
+        Ok(parse_element(&mut chars))
     }
 }
 
-fn parse_input(filename: &str) {
+fn parse_element(element: &mut Chars) -> Distress {
+    let mut result = vec![];
+    let mut token = String::new();
 
-    let input = get_input_string(filename);
-    let lines = input.split_ascii_whitespace().collect_vec();
+    while let Some(current) = element.next() {
+        if current.is_numeric() {
+            token.push(current);
+        } else if current == '[' {
+            let elements = parse_element(element);
+            result.push(elements);
+        } else {
+            if !token.is_empty() {
+                let number = token.parse().unwrap();
+                result.push(Distress::Number(number));
+                token.clear();
+            }
 
-    for a in lines.windows(2) {
+            if current == ']' {
+                break;
+            }
+        }
+    }
+
+    Distress::Array(result)
+}
+
+fn parse_pairs(filename: &str) -> Vec<(Distress, Distress)> {
+    let input: Vec<String> = get_input(filename);
+    let lines = input.iter().filter(|l| !l.is_empty()).collect_vec();
+
+    let mut pairs = vec![];
+
+    for a in lines.chunks(2) {
         let left: Distress = a[0].parse().unwrap();
         let right: Distress = a[1].parse().unwrap();
 
-        println!("{left:?}");
-        println!("{right:?}");
-        println!();
+        pairs.push((left, right));
     }
 
+    pairs
+}
+
+fn parse_packets(filename: &str) -> Vec<Distress> {
+    let input: Vec<String> = get_input(filename);
+    let lines = input.iter().filter(|l| !l.is_empty()).collect_vec();
+
+    lines.iter().map(|elem| elem.parse().unwrap()).collect_vec()
 }
 
 pub fn solve_1(filename: &str) -> Result<String> {
+    let pairs = parse_pairs(filename);
 
-    parse_input(filename);
+    let mut result = vec![];
+    let mut index = 1;
 
+    for (left, right) in pairs.iter() {
+        let ordering = left.cmp(right);
 
-    todo!()
+        match ordering {
+            Ordering::Less => {
+                result.push(index);
+            }
+            Ordering::Equal |
+            Ordering::Greater => {}
+        }
+
+        index += 1;
+    }
+
+    let sum: usize = result.iter().sum();
+    Ok(sum.to_string())
 }
 
 
 pub fn solve_2(filename: &str) -> Result<String> {
-    parse_input(filename);
+    let mut packets = parse_packets(filename);
 
-    todo!()
+    let first_divider: Distress = "[[2]]".parse().unwrap();
+    let second_divider: Distress = "[[6]]".parse().unwrap();
+
+    packets.push(first_divider.clone());
+    packets.push(second_divider.clone());
+
+    let sorted: Vec<&Distress> = packets.iter().sorted().collect_vec();
+
+    let (first_divider_index, _) = sorted.iter().find_position(|b| **b == &first_divider).unwrap();
+    let (second_divider_index, _) = sorted.iter().find_position(|b| **b == &second_divider).unwrap();
+
+    let result = (first_divider_index + 1) * (second_divider_index + 1);
+
+    Ok(result.to_string())
 }
